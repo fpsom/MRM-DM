1f0ce33f2724aae641c5f50505434c5c67866d5c
Fotis E. Psomopoulos <fpsom@issel.ee.auth.gr>
2016-05-05 15:29:29 +0300

    Final version of the script

--- MouseClusteringHeatmap.R
+++ MouseClusteringHeatmap.R
@@ -11,13 +11,7 @@ library(dplyr);
 library(gplots);
 library(RColorBrewer);
 
-
 MouseDataRaw <- read.csv(file="data/Data_Cortex_Nuclear.csv",head=TRUE,sep=";");
-
-summary(MouseDataRaw);
-names(MouseDataRaw);
-attributes(MouseDataRaw);
-
 
 # STEP 1: Restructure data for the clustering process
 # ====================================================
@@ -31,43 +25,42 @@ MouseDataRaw$rowNamesInfo <- paste(MouseDataRaw$MouseID, MouseDataRaw$class, sep
 MouseData_cCSs <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "c-CS-s") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
-
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 MouseData_cCSm <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "c-CS-m") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 MouseData_cSCs <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "c-SC-s") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 MouseData_cSCm <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "c-SC-m") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 MouseData_tCSs <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "t-CS-s") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 MouseData_tCSm <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "t-CS-m") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 MouseData_tSCs <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "t-SC-s") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 MouseData_tSCm <- MouseDataRaw %>%
   na.omit() %>%
   filter(class == "t-SC-m") %>%
-  select(-MouseID, -Genotype, -Treatment, -Behavior, -class);
+  select(-MouseID, -Genotype, -Treatment, -Behavior);
 
 
 # Finally, join all subsets in the final set. The label attribute is assigned as the
@@ -75,15 +68,41 @@ MouseData_tSCm <- MouseDataRaw %>%
 # Final construct: each row has 77 attributes (for the 77 proteins)
 MouseDataClean <- bind_rows(MouseData_cCSs, MouseData_cCSm, MouseData_cSCs, MouseData_cSCm, MouseData_tCSs, MouseData_tCSm, MouseData_tSCs, MouseData_tSCm);
 rownames(MouseDataClean) <- MouseDataClean$rowNamesInfo; 
-MouseDataClean <- select(MouseDataClean, -rowNamesInfo);
+MouseData <- select(MouseDataClean, -rowNamesInfo, -class);
+
+head(MouseData);
+summary(MouseData);
+
+# STEP 2: Define the clustering methods
+# ==========================================
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# You should select both the distance and the clustering method
+# used for better results
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Clustering or rows (i.e. tissues/samples)
+# Distance choices: euclidean (default), maximum, canberra, binary, minkowski, manhattan
+# Cluster method choices: complete (default), single, average, mcquitty, median, centroid, ward.D, ward.D2
+row_distance = dist(as.matrix(MouseData)[c(1:nrow(MouseData)),], method = "euclidean");
+row_cluster = hclust(row_distance, method = "complete");
 
 
-# STEP 2: Initialize the heatmap arrangement
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# You should select both the distance and the clustering method
+# used for better results
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Clustering of columns (i.e. protein expressions)
+# Distance choices: euclidean (default), maximum, canberra, binary, minkowski, manhattan
+# Cluster method choices: complete (default), single, average, mcquitty, median, centroid, ward.D, ward.D2
+col_distance = dist(t(as.matrix(MouseData)[c(1:nrow(MouseData)),]), method = "euclidean");
+col_cluster = hclust(col_distance, method = "complete");
+
+
+# STEP 3: Initialize the heatmap arrangement
 # ==========================================
 
 # Define the color range for the actual expression levels
 my_palette <- colorRampPalette(c("green", "yellow", "orange", "red"))(n = 399);
-
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # You should change the ranges to better fit the scaling you wish
@@ -99,43 +118,6 @@ col_breaks = c(seq(0  ,  0.49,   length=100),    # for green
                seq(0.8,  0.99,   length=100),    # for orange
                seq(1  , 10,      length=100));   # for red
 
-
-# STEP 3: Define the clustering methods
-# ==========================================
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# You should select both the distance and the clustering method
-# used for better results
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Clustering or rows (i.e. tissues/samples)
-# Distance choices: euclidean (default), maximum, canberra, binary, minkowski, manhattan
-# Cluster method choices: complete (default), single, average, mcquitty, median, centroid, ward.D, ward.D2
-row_distance = dist(as.matrix(MouseDataClean)[c(1:nrow(MouseDataClean)),], method = "euclidean");
-row_cluster = hclust(row_distance, method = "complete");
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# You should select both the distance and the clustering method
-# used for better results
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Clustering of columns (i.e. protein expressions)
-# Distance choices: euclidean (default), maximum, canberra, binary, minkowski, manhattan
-# Cluster method choices: complete (default), single, average, mcquitty, median, centroid, ward.D, ward.D2
-col_distance = dist(t(as.matrix(MouseDataClean)[c(1:nrow(MouseDataClean)),]), method = "euclidean");
-col_cluster = hclust(col_distance, method = "complete");
-
-
-
-# STEP 4: Create heatmap
-# ==========================================
-
-# Creates the png image file
-png("MouseProteinExpressionHeatmap.png",   # create PNG for the heat map        
-    width  = 8000,
-    height = 6000,
-    res    = 300,                               # 300 pixels per inch
-    pointsize = 8);                             # smaller font size
-
 # Define a color for each of the 8 different cases
 colorList = c("gray", "blue", "lightsalmon", "orchid", "skyblue", "black", "green", "chartreuse4", "burlywood");
 rowCategories <- c(rep(colorList[1], nrow(MouseData_cCSs)),   # c-CS-s
@@ -149,9 +131,21 @@ rowCategories <- c(rep(colorList[1], nrow(MouseData_cCSs)),   # c-CS-s
 );
 classNames <- c("c-CS-s", "c-CS-m", "c-SC-s", "c-SC-m", "t-CS-s", "t-CS-m", "t-SC-s", "t-SC-m");
 
+
+# STEP 4: Create heatmap
+# ==========================================
+
+# Creates the png image file
+png("MouseProteinExpressionHeatmap.png",   # create PNG for the heat map        
+    width  = 8000,
+    height = 6000,
+    res    = 300,                               # 300 pixels per inch
+    pointsize = 8);                             # smaller font size
+
+
 # Constructs the heatmap with the selected parameters
-heatmap.2(as.matrix(MouseDataClean)[c(1:nrow(MouseDataClean)),],                    # data for heatmap
-          cellnote = as.matrix(MouseDataClean)[c(1:nrow(MouseDataClean)),],         # same data set for cell labels
+heatmap.2(as.matrix(MouseData)[c(1:nrow(MouseData)),],                    # data for heatmap
+          #cellnote = as.matrix(MouseDataClean)[c(1:nrow(MouseDataClean)),],         # same data set for cell labels
           main = "Mouse Protein Expression", # heat map title
           notecol="black",                   # change font color of cell labels to black
           density.info="histogram",          # turns off density plot inside color legend ("histogram","density","none"),
